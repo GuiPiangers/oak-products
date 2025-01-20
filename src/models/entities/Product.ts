b9a3e1043f36c50a9da3093a5f120df23911bc07
GuiPiangers <eduardomartinspiangers@gmail.com>
2025-01-20 16:10:20 +0000

    refactor: change responsibility of validating types to product model

--- src/models/entities/Product.ts
+++ src/models/entities/Product.ts
@@ -18,6 +18,8 @@ export class Product {
     constructor({
         available, description, name, value, id
     }: ProductDTO & {id?: string}){
+        this.validateTypes({ available, description, name, value })
+
         this.id = id ?? generateUUID()
         this.name = name
         this.description = description
@@ -29,5 +31,17 @@ export class Product {
 
     private validateValue(value: number) {
         if(value < 0) throw new Error("O valor precisa ser um número positivo")
+    }
+
+    private validateTypes({available, description, name, value}: ProductDTO){
+        if(!name) throw new Error("O campo nome é obrigatório")
+            if(!value) throw new Error("O campo valor é obrigatório")
+            if(!available) throw new Error("O campo disponível é obrigatório")
+    
+            if(typeof name !== 'string') throw new Error('O campo nome deve ser uma "string"')
+            if(typeof value !== 'number') throw new Error('O campo nome deve ser uma "number"')
+            if(description && typeof description !== 'string') 
+                throw new Error('O campo descrição deve ser uma "string"')
+            if(typeof available !== "boolean") throw new Error('O campo desponível deve ser uma "number"')
     }
 }
