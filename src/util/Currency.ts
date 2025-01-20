1be4f63373a96a1b7083fd693273f6afd558de12
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-01-20 20:09:51 -0300

    feat: create Currency utility class

---
+++ src/util/Currency.ts
@@ -0,0 +1,20 @@
+export class Currency {
+    static format(value: string | number) {
+      const stingValue =
+        typeof value === 'number' ? String(value.toFixed(2)) : value
+  
+      return stingValue
+        .replace(/\D/g, '')
+        .replace(/^0+/, '')
+        .replace(/(\d)(\d{2})$/, '$1,$2')
+        .replace(/(?=(\d{3})+(\D))\B/g, '.')
+    }
+
+    static unFormat(value: string ) {
+      return +value
+        .replace(/\D/g, '')
+        .replace('.', '')
+        .replace(',', '.')
+    }
+  }
+  
