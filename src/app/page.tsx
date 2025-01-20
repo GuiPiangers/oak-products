cc9b878c82c640795a1f627e9a5b63bb8c25802a
GuiPiangers <eduardomartinspiangers@gmail.com>
2025-01-20 20:23:27 +0000

    feat: create home page template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import ProductTable from "@/components/products/ProductTable";
+import { Box } from "@/components/ui/Box";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <section>
-
+    <section className="w-full flex flex-col gap-4 items-center bg-zinc-50 h-full px-4 py-8">
+      <Box className="flex justify-between w-full max-w-screen-md gap-2 items-center">
+        <h1 className="text-xl font-bold">Lista de produtos OAK</h1>
+        <Button>Novo Produto</Button>
+      </Box>
+      <Box className="w-full max-w-screen-md overflow-y-auto">
+        <ProductTable productList={[]}/>
+      </Box>
     </section>
   );
 }
