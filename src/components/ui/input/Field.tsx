5707fcfc0c190424b7c57b5c11ebd61ad92a4a4f
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-01-20 21:55:26 -0300

    style: fix error styles

--- src/components/ui/input/Field.tsx
+++ src/components/ui/input/Field.tsx
@@ -6,11 +6,12 @@ import useIdContext from "@/hooks/useIdContext"
 import { cva, VariantProps } from "class-variance-authority"
 
 const fieldStyles = cva(
-  "flex h-10 w-full rounded-md border border-zinc-200 bg-white px-3 py-2 text-base ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-zinc-950 placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm dark:border-zinc-800 dark:bg-zinc-950 dark:ring-offset-zinc-950 dark:file:text-zinc-50 dark:placeholder:text-zinc-400 dark:focus-visible:ring-zinc-300",
+  "flex h-10 w-full rounded-md border  px-3 py-2 text-base ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-zinc-950 placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm dark:border-zinc-800 dark:bg-zinc-950 dark:ring-offset-zinc-950 dark:file:text-zinc-50 dark:placeholder:text-zinc-400 dark:focus-visible:ring-zinc-300",
   {
   variants: {
     error: {
       true: 'bg-red-50 text-red-600 border-red-600 placeholder:text-red-300 autofill:shadow-[0_0_0px_1000px_inset] autofill:shadow-red-50 autofill:focus:shadow-white',
+      false: 'border-zinc-200 bg-white'
     },
   },
 })
