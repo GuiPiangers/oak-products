881185b0b5216e6c9a3789a129c900b49204d9a7
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-01-20 21:20:04 -0300

    feat: create Message Input component

---
+++ src/components/ui/input/Message.tsx
@@ -0,0 +1,26 @@
+import { cva, VariantProps } from 'class-variance-authority'
+import { HTMLAttributes } from 'react'
+
+const messageStyle = cva('flex items-start gap-1 text-xs',{
+  variants: {
+    error: {
+      true: 'text-red-600',
+    },
+  },
+})
+
+type Variants = VariantProps<typeof messageStyle>
+type InputMessageProps = HTMLAttributes<HTMLInputElement> & Variants
+
+export default function Message({
+  className,
+  error,
+  children,
+  ...props
+}: InputMessageProps) {
+  return (
+    <span {...props} className={messageStyle({ error, className })}>
+      {children}
+    </span>
+  )
+}
