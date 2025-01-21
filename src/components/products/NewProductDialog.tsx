5199847ed1e3e896b528253b19be0cd5ed4282f8
Guilherme Piangers <eduardomartinspiangers@gmail.com>
2025-01-20 21:23:03 -0300

    fix: add responseError type

--- src/components/products/NewProductDialog.tsx
+++ src/components/products/NewProductDialog.tsx
@@ -11,6 +11,8 @@ import {
     DialogFooter
 } from "../ui/dialog/dialog";
 import { Input } from "../ui/input";
+import { useState } from "react";
+import { productController } from "@/controllers/products";
 
 type NewProductDialogProps = {
     children: React.ReactNode;
@@ -18,6 +20,18 @@ type NewProductDialogProps = {
 } 
 
 export default function NewProductDialog({children, asChild}: NewProductDialogProps) {
+    const [name, setName] = useState('');
+    const [nameError, setNameError] = useState<string>();
+
+    const [description, setDescription] = useState('');
+    const [descriptionError, setDescriptionError] = useState<string>();
+
+    const [value, setValue] = useState('00,00');
+    const [valueError, setValueError] = useState<string>();
+
+    const [available, setAvailable] = useState(true);
+    const [availableError, setAvailableError] = useState<string>();
+
     return (
     <Dialog>
         <DialogTrigger asChild={asChild}>
@@ -31,24 +45,62 @@ export default function NewProductDialog({children, asChild}: NewProductDialogPr
 
             <form className="flex flex-col gap-4">
                 <Input.Root>
-                    <Input.Label>Nome</Input.Label>
-                    <Input.Field />
+                    <Input.Label required>Nome</Input.Label>
+                    <Input.Field 
+                        value={name}
+                        error={!!nameError}
+                        onChange={e => setName(e.target.value)} 
+                    />
+                    {nameError && (
+                            <Input.Message error>{nameError}</Input.Message>
+                    )}
                 </Input.Root>
 
                 <Input.Root>
                     <Input.Label>Descrição</Input.Label>
-                    <Input.Field />
+                    <Input.Field 
+                        value={description}
+                        error={!!descriptionError}
+                        onChange={(e) => setDescription(e.target.value)} 
+                    />
+                    {descriptionError && (
+                        <Input.Message error>{descriptionError}</Input.Message>
+                    )}
                 </Input.Root>
 
                 <Input.Root>
-                    <Input.Label>Valor</Input.Label>
-                    <Input.Field onChange={e => 
-                        e.target.value = Currency.format(e.target.value)
-                    } />
+                    <Input.Label required>Valor</Input.Label>
+                    <Input.Field
+                        value={value}
+                        onChange={e => setValue(Currency.format(e.target.value))}
+                        error={!!valueError}
+                    />
+                    {valueError && (
+                        <Input.Message error>{valueError}</Input.Message>
+                    )}
+
+                </Input.Root>
+
+                <Input.Root>
+                    <Input.Label required>Valor</Input.Label>
+                    <Input.Switch
+                        checked={available}
+                        onCheckedChange={(e) => setAvailable(e)}
+                    />
+                    {availableError && (
+                        <Input.Message error>{availableError}</Input.Message>
+                    )}
                 </Input.Root>
 
                 <DialogFooter>
-                    <Button>
+                    <Button onClick={async ()=>{
+                        const response = await productController.create({
+                            name,
+                            description,
+                            value: Currency.unFormat(value),
+                            available
+                        })
+                    }}>
                         Salvar                        
                     </Button>
                 </DialogFooter>
